706bcdad9c1d32d96a254c99b1958d2b7420f618
jt-lab <53636417+jt-lab@users.noreply.github.com>
2020-06-16 21:35:02 +0800

    Update draw.py

--- MyQR/mylibs/draw.py
+++ MyQR/mylibs/draw.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from PIL import Image
+from PIL import Image, ImageColor
 import os
 
-def draw_qrcode(abspath, qrmatrix):
+def draw_qrcode(abspath, qrmatrix,background, foreground):
     unit_len = 3
     x = y = 4*unit_len
-    pic = Image.new('1', [(len(qrmatrix)+8)*unit_len]*2, 'white')
+    pic = Image.new('1', [(len(qrmatrix)+8)*unit_len]*2, background)
     
     for line in qrmatrix:
         for module in line:
             if module:
-                draw_a_black_unit(pic, x, y, unit_len)
+                draw_a_black_unit(pic, x, y, unit_len, ImageColor.getcolor(foreground, "RGB"))
             x += unit_len
         x, y = 4*unit_len, y+unit_len
 
@@ -19,7 +19,7 @@ def draw_qrcode(abspath, qrmatrix):
     pic.save(saving)
     return saving
     
-def draw_a_black_unit(p, x, y, ul):
+def draw_a_black_unit(p, x, y, ul, color):
     for i in range(ul):
         for j in range(ul):
-            p.putpixel((x+i, y+j), 0)
+            p.putpixel((x+i, y+j), color)
